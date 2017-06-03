4585c9f5a3b92bdcf0982b978be5e7ed37082b45
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-06-04 01:37:03 +0300

    'equals' updates

--- aikolesnikov/src/main/java/Group.java
+++ aikolesnikov/src/main/java/Group.java
@@ -7,6 +7,30 @@ public class Group {
     protected int size;
     protected Student[] students;
 
+    public Group() {
+        students = new Student[INIT_GR_SIZE];
+        size = 0;
+    }
+
+    public Group(int id) {
+        this.id = id;
+        this.students = new Student[INIT_GR_SIZE];
+        size = 0;
+    }
+
+    public Group(int id, Student[] students) {
+        this.id = id;
+        if (students == null) {
+            this.students = new Student[INIT_GR_SIZE];
+            this.size = 0;
+        } else {
+            this.students = students;
+            for (Student student : students) {
+                if (!(student == null)) size++;
+            }
+        }
+    }
+
     public int getId() {
         return id;
     }
@@ -33,30 +57,6 @@ public class Group {
 
     public static int getInitGrSize() {
         return INIT_GR_SIZE;
-    }
-
-    public Group() {
-        students = new Student[INIT_GR_SIZE];
-        size = 0;
-    }
-
-    public Group(int id) {
-        this.id = id;
-        this.students = new Student[INIT_GR_SIZE];
-        size = 0;
-    }
-
-    public Group(int id, Student[] students) {
-        this.id = id;
-        if (students == null) {
-            this.students = new Student[INIT_GR_SIZE];
-            this.size = 0;
-        } else {
-            this.students = students;
-            for (Student student : students) {
-                if (!(student == null)) size++;
-            }
-        }
     }
 
     public boolean addStudent(Student student) {
@@ -128,7 +128,6 @@ public class Group {
             }
         }
     }
-
 
     @Override
     public String toString() {
