9505861743fafb8ed944da5a8700a5bc21324cbd
AlexanderShpilka <sandro.bot.evil@gmail.com>
2017-06-08 20:38:09 +0300

    LinkedListMethods

--- AlexanderShpilka/src/main/java/week4/day2/LinkedListMethods.java
+++ AlexanderShpilka/src/main/java/week4/day2/LinkedListMethods.java
@@ -1,7 +1,22 @@
 package week4.day2;
 
-/**
- * Created by Alex on 05.06.2017.
- */
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class LinkedListMethods {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("One");
+        linkedList.add("Two");
+        linkedList.add("Three");
+        linkedList.add("Four");
+        System.out.println(Arrays.asList(linkedList));
+
+        linkedList.offerLast("pop");
+
+//        linkedList.offerFirst("Zero");
+        System.out.println(Arrays.asList(linkedList));
+
+//        System.out.println(linkedList.peekFirst());
+    }
 }
