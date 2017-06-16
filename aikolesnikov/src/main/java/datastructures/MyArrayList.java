905d86c150a5e55380791afeea73a49e74104ecd
Artyom Kolesnikov <artyom.kolesnikov@worldapp.com>
2017-06-16 19:24:46 +0300

    LinkedList

--- aikolesnikov/src/main/java/datastructures/MyArrayList.java
+++ aikolesnikov/src/main/java/datastructures/MyArrayList.java
@@ -6,35 +6,35 @@ import java.util.Iterator;
 /**
  * MyArrayLisr Class
  */
-public class MyArrayList implements MyList {
+public class MyArrayList<T> implements MyList<T> {
 
     private static final int INIT_SIZE = 10;
 
     private int size;
-    Object[] ar;
+    T[] ar;
 
     public MyArrayList() {
         this.size = INIT_SIZE;
-        this.ar = new Object[INIT_SIZE];
+        this.ar = (T[]) new Object[INIT_SIZE];
     }
 
-    public MyArrayList(MyArrayList o) {
+    public MyArrayList(MyArrayList<T> o) {
         this.size = o.size;
         this.ar = o.ar;
     }
 
-    public MyArrayList(Object[] ar) {
+    public MyArrayList(T[] ar) {
         this.size = ar.length;
         this.ar = ar;
     }
 
     public MyArrayList(int initialCapacity) {
         this.size = 0;
-        this.ar = new Object[initialCapacity];
+        this.ar = (T[]) new Object[initialCapacity];
     }
 
     @Override
-    public boolean add(Object o, int index) {
+    public boolean add(T o, int index) {
 
         if ((index < 0) || (index > ar.length - 1)) return false;
 
@@ -48,30 +48,30 @@ public class MyArrayList implements MyList {
     }
 
     @Override
-    public Object get(int index) {
+    public T get(int index) {
         if ((index < 0) || (index > ar.length - 1)) return null;
         return ar[index];
     }
 
     @Override
-    public Object remove(int index) {
+    public T remove(int index) {
         if ((index < 0) || (index > size - 1)) return null;
 
-        Object tmpObj = ar[index];
+        T tmpObj = ar[index];
         System.arraycopy(ar, index + 1, ar, index, size - index - 1);
         ar[--size] = null;
         return tmpObj;
     }
 
     @Override
-    public boolean set(Object o, int index) {
+    public boolean set(T o, int index) {
         if ((index < 0) || (index > size - 1)) return false;
         ar[index] = o;
         return true;
     }
 
     @Override
-    public boolean add(Object o) {
+    public boolean add(T o) {
         if (size == ar.length) {
             ensureCapacity();
         }
@@ -80,13 +80,13 @@ public class MyArrayList implements MyList {
     }
 
     private void ensureCapacity() {
-        Object[] newAr = new Object[size * 15 / 10];
+        T[] newAr = (T[]) new Object[size * 15 / 10];
         System.arraycopy(ar, 0, newAr, 0, size);
         ar = newAr;
     }
 
     @Override
-    public boolean contains(Object o) {
+    public boolean contains(T o) {
         return indexOf(o) != -1;
     }
 
@@ -109,7 +109,7 @@ public class MyArrayList implements MyList {
     }
 
     @Override
-    public boolean remove(Object o) {
+    public boolean remove(T o) {
         int ind = indexOf(o);
 
         if (ind != -1) {
@@ -124,7 +124,7 @@ public class MyArrayList implements MyList {
     public int size() {
         return size;
     }
-    public int indexOf(Object o) {
+    public int indexOf(T o) {
         if (o == null){
             for (int i = 0; i < size; i++) {
                 if (ar[i]==null) return i;
@@ -137,7 +137,7 @@ public class MyArrayList implements MyList {
         return -1;
     }
 
-    public Iterator iterator() {
+    public Iterator<T> iterator() {
         return null;
     }
 }
